cea982cebcce74e3e1037e75331558a894c0a006
Seondeok Kim <guts0808@gmail.com>
2022-10-14 17:44:41 +0900

    init

---
+++ github_userlist/github_userlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_userlistApp.swift
+//  github_userlist
+//
+//  Created by 金善徳 on 2022/10/14.
+//
+
+import SwiftUI
+
+@main
+struct github_userlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
